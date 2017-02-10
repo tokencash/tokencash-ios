b8de4d0c5f1534a7b9009e3a2f230d778eaf0a5e
Luis Perez <luis@MacBook-Pro-de-Luis.local>
2017-02-10 11:45:18 -0600

    Initial Commit

---
+++ TokencashAPI/TokencashAPI.h
@@ -0,0 +1,19 @@
+//
+//  TokencashAPI.h
+//  TokencashAPI
+//
+//  Created by Luis Perez on 10/02/17.
+//  Copyright © 2017 stx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TokencashAPI.
+FOUNDATION_EXPORT double TokencashAPIVersionNumber;
+
+//! Project version string for TokencashAPI.
+FOUNDATION_EXPORT const unsigned char TokencashAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TokencashAPI/PublicHeader.h>
+
+
